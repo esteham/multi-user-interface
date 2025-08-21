c33e088591098122720f6bfbccc56452650cff3f
Esteham H. Zihad ansari <eshasan1287005@gmail.com>
2025-08-21 23:26:26 +0600

    create seeder,added dummy and fake data insert in users table successfully

--- database/seeders/UsersTableseeder.php
+++ database/seeders/UsersTableseeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+Use Illuminate\Support\Facades\Hash;
+Use DB;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,6 +14,36 @@ class UsersTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('users')->insert([
+            //Admin
+            [
+                'name' => 'Admin',
+                'username' => 'admin',
+                'email' => 'admin@mail.com',
+                'password' => Hash::make('1234'),
+                'role' => 'admin',
+                'status' => 'active',
+            ],
+
+            //Agent
+            [
+                'name' => 'Agent',
+                'username' => 'agent',
+                'email' => 'agent@mail.com',
+                'password' => Hash::make('1234'),
+                'role' => 'agent',
+                'status' => 'active',
+            ],
+
+            //User
+            [
+                'name' => 'User',
+                'username' => 'user',
+                'email' => 'user@mail.com',
+                'password' => Hash::make('1234'),
+                'role' => 'user',
+                'status' => 'active',
+            ]
+        ]);
     }
 }
